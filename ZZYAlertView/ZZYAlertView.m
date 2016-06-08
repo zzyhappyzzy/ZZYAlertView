07d25aba560499a4e3a1c4c97e503566496123f0
zhenchy <zhenchaoyang@haizitong.com>
2016-06-08 17:00:51 +0800

    fix 0.0.2 bug

--- ZZYAlertView/ZZYAlertView.m
+++ ZZYAlertView/ZZYAlertView.m
@@ -12,6 +12,10 @@
 #define SCREEN_WIDTH [[UIScreen mainScreen] bounds].size.width
 #define SCREEN_HEIGHT [[UIScreen mainScreen] bounds].size.height
 
+@implementation AttributeTextInfo
+
+@end
+
 @interface ZZYAlertView() {
     NSTimer *autoHideTimer;
 }
